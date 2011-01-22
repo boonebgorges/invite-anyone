3c17e81b9d6eeb1c68f91b3063058aaf5fa610a6
Boone Gorges <boonebgorges@gmail.com>
2011-01-22 14:08:24 -0500

    Some cs cleanup

--- by-email/cloudsponge-integration.php
+++ by-email/cloudsponge-integration.php
@@ -24,29 +24,10 @@ class Cloudsponge_Integration {
 	 * @since 0.8
 	 */
 	function __construct() {
-		add_action( 'invite_anyone_setup_nav', array( $this, 'setup_nav' ) );
-		
-		add_action( 'wp', array( $this, 'catch_popup' ), 1 );
-		add_action( 'invite_anyone_after_addresses', array( $this, 'import_markup_1' ) );
-		
-		
-		// Hack to keep the Import Contacts screen out of the nav
-		add_filter( 'bp_get_options_nav_import-contacts', array( $this, 'hide_subnav' ) );
+		add_action( 'invite_anyone_after_addresses', array( $this, 'import_markup' ) );
 	}
 
-	function catch_popup() {
-		global $bp;
-		
-		$file = '';
-		
-		if ( 'import-contacts' == $bp->current_action && BP_INVITE_ANYONE_SLUG == $bp->current_component ) {
-				
-			require_once( WP_PLUGIN_DIR . '/invite-anyone/lib/cloudsponge-lib-php/popup.php' );
-			die();
-		}
-	}
-	
-	function import_markup_1() {
+	function import_markup() {
 		?>		
 		
 		
@@ -62,70 +43,6 @@ class Cloudsponge_Integration {
 <textarea id="contact_list" style="width:450px;height:82px"></textarea>
 		
 		
-		
-			<p><?php _e( 'Or, import addresses from your address book', 'bp-invite-anyone' ) ?></p>
-			<div class="address-book-import">
-				<a href='?service=yahoo'       onclick="return open_popup('yahoo', true);">Yahoo!</a>
-				<a href='?service=windowslive' onclick="return open_popup('windowslive', true)">Windows Live</a>
-				<a href='?service=gmail'       onclick="return open_popup('gmail', true)">Gmail</a>
-				<a href='?service=aol'         onclick="return show_u_p_fields('aol');">AOL</a>
-				<a href='?service=plaxo'       onclick="return show_u_p_fields('plaxo');">Plaxo</a>
-				<a href='?service=outlook'     onclick="return open_popup('outlook', false)">Outlook</a>
-				<a href='?service=addressbook' onclick="return open_popup('addressbook', false)">Mac Address Book</a>
-			</div>
-			
-			<form id="u_p_inputs" style="display:none;">
-			  Username: <input type='text' id='username'/><br />
-			  Password: <input type='password' id='password'/><br />
-			  <input type='submit' onclick="return open_popup(input_service, false, document.getElementById('username').value, document.getElementById('password').value);" name='submit'/>
-			</form>
-			
-			<script src="prototype.js" type="text/javascript"></script>
-			<script type="text/javascript">
-			var input_service;
-			function show_u_p_fields(service_name) {
-			  input_service = service_name;
-			  $('u_p_inputs').show();
-			  return false;
-			}
-			function open_popup(service, focus, username, password, url) {
-			  if (url == undefined) { url = '<?php echo bp_loggedin_user_domain() . BP_INVITE_ANYONE_SLUG  ?>/import-contacts'; }
-			  url = url + '?service=' + service;
-			  if (username != null) {
-			    url = url + '&username=' + username + '&password=' + password;
-			  }
-			
-			  popup_height = '300';
-			  popup_width = '500';
-			  
-			  if (service == 'yahoo') {
-			    popup_height = '500';
-			    popup_width = '500';
-			  } else if (service == 'gmail') {
-			    popup_height = '600';
-			    popup_width = '987';
-			  } else if (service == 'windowslive') {
-			    popup_height = '600';
-			    popup_width = '987';
-			  } else if (service == 'aol' || service == 'plaxo') {
-			    popup_height = '600';
-			    popup_width = '987';
-			  }
-			  
-			  popup = window.open(url, "_popupWindow", 'height='+popup_height+',width='+popup_width+',location=yes,menubar=no,resizable=no,status=no,toolbar=no');
-			  if (focus) {
-			    popup.focus();
-			  }
-			  else {
-			    window.focus();
-			  }
-			  
-			  // wait for the popup window to indicate the import_id to start checking for events...
-			  
-			  return (undefined == popup);
-			}
-			</script>
-
 		<?php
 	}
 
