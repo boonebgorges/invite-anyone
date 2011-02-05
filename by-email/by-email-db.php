ae741ad8956fe2425b2f2d05c90fb88bb7010b6b
Boone Gorges <boonebgorges@gmail.com>
2011-02-05 11:43:59 -0500

    Defines post type and taxonomy

--- by-email/by-email-db.php
+++ by-email/by-email-db.php
@@ -1,6 +1,95 @@
 <?php
 /* Invite Anyone database functions */
 
+class Invite_Anyone_Data {
+	var $post_type_name;
+	var $invitee_tax_name;
+	
+	/**
+	 * PHP4 Constructor
+	 *
+	 * @package Invite Anyone
+	 * @since 0.8
+	 */
+	function bpgr_settings() {
+		$this->construct();
+	}
+	
+	/**
+	 * PHP5 Constructor
+	 *
+	 * @package Invite Anyone
+	 * @since 0.8
+	 */
+	function __construct() {
+		// Define the post type name used throughout
+		$this->post_type_name = apply_filters( 'invite_anyone_post_type_name', 'ia_invites' );
+		
+		// Define the invitee tax name used throughout
+		$this->invitee_tax_name = apply_filters( 'invite_anyone_invitee_tax_name', 'ia_invitees' );
+		
+		// Hooks into the 'init' action to register our WP custom post type and tax
+		add_action( 'init', array( $this, 'register_post_type' ) );	
+	}
+
+	/**
+	 * Registers Invite Anyone's post type
+	 *
+	 * @package BuddyPress Docs
+	 * @since 1.0
+	 */
+	function register_post_type() {
+		// Define the labels to be used by the post type		
+		$post_type_labels = apply_filters( 'invite_anyone_post_type_labels', array(
+			'name' 			=> _x( 'BuddyPress Invitations', 'post type general name', 'bp-invite-anyone' ),
+			'singular_name' 	=> _x( 'Invitation', 'post type singular name', 'bp-invite-anyone' ),
+			'add_new' 		=> _x( 'Add New', 'add new', 'bp-invite-anyone' ),
+			'add_new_item' 		=> __( 'Add New Invitation', 'bp-invite-anyone' ),
+			'edit_item' 		=> __( 'Edit Invitation', 'bp-invite-anyone' ),
+			'new_item' 		=> __( 'New Invitation', 'bp-invite-anyone' ),
+			'view_item' 		=> __( 'View Invitation', 'bp-invite-anyone' ),
+			'search_items' 		=> __( 'Search Invitation', 'bp-invite-anyone' ),
+			'not_found' 		=>  __( 'No Invitations found', 'bp-invite-anyone' ),
+			'not_found_in_trash' 	=> __( 'No Invitations found in Trash', 'bp-invite-anyone' ),
+			'parent_item_colon' 	=> ''
+		), &$this );
+	
+		// Register the invitation post type
+		register_post_type( $this->post_type_name, apply_filters( 'invite_anyone_post_type_args', array(
+			'label' 	=> __( 'BuddyPress Invitations', 'bp-invite-anyone' ),
+			'labels' 	=> $post_type_labels,
+			'public' 	=> false,
+			'_builtin' 	=> false,
+			'show_ui' 	=> true,
+			'hierarchical' 	=> false,
+			'supports' 	=> array( 'title', 'editor' )
+		), &$this ) );
+		
+		// Define the labels to be used by the invitee taxonomy
+		$invitee_labels = apply_filters( 'invite_anyone_invitee_labels', array(
+			'name' 		=> __( 'Invitees', 'bp-invite-anyone' ),
+			'singular_name' => __( 'Invitee', 'bp-invite-anyone' ),
+			'search_items' 	=>  __( 'Search Invitees', 'bp-invite-anyone' ),
+			'all_items' 	=> __( 'All Invitees', 'bp-invite-anyone' ),
+			'edit_item' 	=> __( 'Edit Invitee', 'bp-invite-anyone' ), 
+			'update_item' 	=> __( 'Update Invitee', 'bp-invite-anyone' ),
+			'add_new_item' 	=> __( 'Add New Invitee', 'bp-invite-anyone' ),
+			'new_item_name' => __( 'New Invitee Name', 'bp-invite-anyone' ),
+			'menu_name' 	=> __( 'Invitee' ),
+		), &$this );
+		
+		// Register the invitee taxonomy
+		register_taxonomy( $this->invitee_tax_name, $this->post_type_name, apply_filters( 'invite_anyone_invitee_tax_args', array(
+			'label'		=> __( 'Invitees', 'bp-invite-anyone' ),
+			'labels' 	=> $invitee_labels,
+			'hierarchical' 	=> false,
+			'show_ui' 	=> true,
+		), &$this ) );
+	}
+}
+
+$invite_anyone_data = new Invite_Anyone_Data;
+
 function invite_anyone_create_table() {
 	global $wpdb;
 	
