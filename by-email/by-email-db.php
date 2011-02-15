73d38c42b3c33ada2f6e40677b8890e776764f2f
boonebgorges <boonebgorges@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-15 15:20:06 +0000

    Sneaking Greek translation into 0.7.1 release. Props Lena Stergatou
    git-svn-id: http://svn.wp-plugins.org/invite-anyone/trunk@345903 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ by-email/by-email-db.php
@@ -0,0 +1,154 @@
+<?php
+/* Invite Anyone database functions */
+
+function invite_anyone_create_table() {
+	global $wpdb;
+	
+	$table_name = $wpdb->base_prefix . 'bp_invite_anyone';  
+  
+	if ( get_option('bp_invite_anyone_ver') != BP_INVITE_ANYONE_VER ) {
+		$sql = "CREATE TABLE {$table_name} (
+			id bigint(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+			inviter_id bigint(20) NOT NULL,
+			email varchar(75) NOT NULL,
+			message longtext NOT NULL,
+			group_invitations longtext,
+			date_invited datetime NOT NULL,
+			is_joined tinyint(1) NOT NULL,
+			date_joined datetime,
+			is_opt_out tinyint(1) NOT NULL,
+			is_hidden tinyint(1) NOT NULL
+			) {$charset_collate};";   		
+	
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+		dbDelta($sql);
+
+		update_option( 'bp_invite_anyone_ver', BP_INVITE_ANYONE_VER );
+	}
+}
+
+function invite_anyone_record_invitation( $inviter_id, $email, $message, $groups ) {
+	global $wpdb, $bp;
+	
+	$group_invitations = maybe_serialize( $groups );
+	$date_invited = gmdate( "Y-m-d H:i:s" );
+	$is_joined = 0;
+	
+	$sql = $wpdb->prepare( "INSERT INTO {$bp->invite_anyone->table_name} ( inviter_id, email, message,  group_invitations, date_invited, is_joined ) VALUES ( %d, %s, %s, %s, %s, %d )", $inviter_id, $email, $message, $group_invitations, $date_invited, $is_joined );
+			
+	if ( !$wpdb->query($sql) )
+		return false;
+	
+	return true;
+}
+
+function invite_anyone_get_invitations_by_inviter_id( $id, $sort_by = false, $order = false ) {
+	global $wpdb, $bp;
+		
+	$sql = $wpdb->prepare( "SELECT * FROM {$bp->invite_anyone->table_name} WHERE inviter_id = %s AND is_hidden = 0", $id );
+	
+	switch ( $sort_by ) {
+		case 'date_invited' :
+			$sql .= ' ORDER BY date_invited';
+			break;
+		case 'date_joined' :
+			$sql .= ' ORDER BY date_joined';
+			break;
+		case 'email' :
+			$sql .= ' ORDER BY email';
+			break;	
+	}
+	
+	if ( $order )
+		$sql .= ' ' . $order;
+	
+	$results = $wpdb->get_results($sql);
+	return $results;	
+
+}
+
+function invite_anyone_get_invitations_by_invited_email( $email ) {
+	global $wpdb, $bp;
+		
+	$sql = $wpdb->prepare( "SELECT * FROM {$bp->invite_anyone->table_name} WHERE email = %s", $email );
+	
+	$results = $wpdb->get_results($sql);
+	return $results;	
+}
+
+function invite_anyone_clear_sent_invite( $args ) {
+	global $wpdb, $bp;
+	
+	/* Accepts arguments: array(
+		'inviter_id' => id number of the inviter, (required)
+		'clear_id' => id number of the item to be cleared,
+		'type' => accepted, unaccepted, or all
+		
+			); */
+	
+	extract( $args );
+	
+	if ( !$inviter_id )
+		return false;
+	
+	if ( $clear_id )
+		$sql = $wpdb->prepare( "UPDATE {$bp->invite_anyone->table_name} SET is_hidden = 1 WHERE id = %d", $clear_id );
+	else if ( $type == 'accepted' )
+		$sql = $wpdb->prepare( "UPDATE {$bp->invite_anyone->table_name} SET is_hidden = 1 WHERE inviter_id = %d AND is_joined = 1", $inviter_id );
+	else if ( $type == 'unaccepted' )
+		$sql = $wpdb->prepare( "UPDATE {$bp->invite_anyone->table_name} SET is_hidden = 1 WHERE inviter_id = %d AND is_joined = 0", $inviter_id );
+	else if ( $type == 'all' )
+		$sql = $wpdb->prepare( "UPDATE {$bp->invite_anyone->table_name} SET is_hidden = 1 WHERE inviter_id = %d", $inviter_id );
+	else
+		return false;	
+	
+	if ( !$wpdb->query($sql) )
+		return false;
+	
+	return true;
+
+}
+
+function invite_anyone_mark_as_joined( $email ) {
+	global $wpdb, $bp;
+	
+	$is_joined = 1;
+	$date_joined = gmdate( "Y-m-d H:i:s" );
+		
+	$sql = $wpdb->prepare( "UPDATE {$bp->invite_anyone->table_name} SET is_hidden = 0, is_joined = %d, date_joined = %s WHERE email = %s", $is_joined, $date_joined, $email ); 
+	
+	if ( !$wpdb->query($sql) )
+		return false;
+	
+	return true;
+}
+
+function invite_anyone_check_is_opt_out( $email ) {
+	global $wpdb, $bp;
+	
+	$sql = $wpdb->prepare( "SELECT * FROM {$bp->invite_anyone->table_name} WHERE email = %s AND is_opt_out = 1", $email );
+	
+	$results = $wpdb->get_results($sql);
+	
+	if ( !$results )
+		return false;
+	
+	return true;
+}
+
+
+function invite_anyone_mark_as_opt_out( $email ) {
+	global $wpdb, $bp;
+	
+	$is_opt_out = 1;
+	
+	$table_name = $wpdb->base_prefix . 'bp_invite_anyone';  
+	
+	$sql = $wpdb->prepare( "UPDATE {$table_name} SET is_opt_out = %d WHERE email = %s", $is_opt_out, $email );
+	
+	if ( !$wpdb->query($sql) )
+		return false;
+	
+	return true;
+}
+?>
---
+++ invite-anyone.php
@@ -0,0 +1,78 @@
+<?php
+
+/*
+Plugin Name: Invite Anyone
+Plugin URI: http://teleogistic.net/code/buddypress/invite-anyone/
+Description: Allows group admins to invite any BuddyPress member to a group, whether or not they are friends
+Version: 0.7.1
+Author: Boone Gorges
+Author URI: http://teleogistic.net
+*/
+
+define( 'BP_INVITE_ANYONE_VER', '0.7' );
+
+if ( !defined( 'BP_INVITE_ANYONE_SLUG' ) )
+	define( 'BP_INVITE_ANYONE_SLUG', 'invite-anyone' );
+
+/* Only load the BuddyPress plugin functions if BuddyPress is loaded and initialized. */
+function invite_anyone_init() {
+	if ( function_exists( 'bp_is_active' ) ) {
+		if ( bp_is_active( 'groups' ) )
+			require( dirname( __FILE__ ) . '/group-invites/group-invites.php' );
+	} else if ( function_exists( 'groups_install' ) ) {
+		require( dirname( __FILE__ ) . '/group-invites/group-invites.php' );
+	}
+
+	require( dirname( __FILE__ ) . '/by-email/by-email.php' );
+
+	if ( is_admin() )
+		require( dirname( __FILE__ ) . '/admin/admin-panel.php' );
+}
+add_action( 'bp_include', 'invite_anyone_init' );
+
+if ( function_exists( 'bp_post_get_permalink' ) )
+	require( dirname( __FILE__ ) . '/group-invites/group-invites.php' );
+
+
+function invite_anyone_locale_init () {
+	$plugin_dir = basename(dirname(__FILE__));
+	$locale = get_locale();
+	$mofile = WP_PLUGIN_DIR . "/invite-anyone/languages/invite-anyone-$locale.mo";
+
+      if ( file_exists( $mofile ) )
+      		load_textdomain( 'bp-invite-anyone', $mofile );
+}
+add_action ('plugins_loaded', 'invite_anyone_locale_init');
+
+
+
+function invite_anyone_activation() {
+	require( dirname( __FILE__ ) . '/by-email/by-email-db.php' );
+	invite_anyone_create_table();
+
+	if ( !$iaoptions = get_option( 'invite_anyone' ) )
+		$iaoptions = array();
+
+	if ( !$iaoptions['max_invites'] )
+		$iaoptions['max_invites'] = 5;
+
+	if ( !$iaoptions['allow_email_invitations'] )
+		$iaoptions['allow_email_invitations'] = 'all';
+
+	if ( !$iaoptions['message_is_customizable'] )
+		$iaoptions['message_is_customizable'] = 'yes';
+
+	if ( !$iaoptions['subject_is_customizable'] )
+		$iaoptions['subject_is_customizable'] = 'no';
+
+	if ( !$iaoptions['can_send_group_invites_email'] )
+		$iaoptions['can_send_group_invites_email'] = 'yes';
+		
+	if ( !$iaoptions['bypass_registration_lock'] )
+		$iaoptions['bypass_registration_lock'] = 'yes';
+
+	update_option( 'invite_anyone', $iaoptions );
+}
+register_activation_hook( __FILE__, 'invite_anyone_activation' );
+
+?>
