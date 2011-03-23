442973980be1ad60bcaa9ff044832562fdfd5feb
boonebgorges <boonebgorges@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-23 17:19:06 +0000

    Moves Cloudsponge init js into an external js file so that it can be properly enqueued and dependent on address_books.js
    git-svn-id: http://plugins.svn.wordpress.org/invite-anyone/trunk@363815 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ by-email/cloudsponge-js.js
@@ -0,0 +1,26 @@
+csInit( { 	
+	domain_key:"<?php echo esc_html( $this->key ) ?>",
+	referrer: 'invite-anyone',
+	afterSubmitContacts:function(contacts) {
+		var emails = [];
+		var contact, name, email;
+		for(var i=0; i < contacts.length; i++) {
+			contact = contacts[i];
+			name = contact.fullName();
+			email = contact.selectedEmail();
+			emails.push(email);
+		}
+		
+		var textarea = document.getElementById('invite-anyone-email-addresses');
+		/* Strip any manually entered whitespace */
+		var already_emails = textarea.value.replace(/^\s+|\s+$/g,"");
+		
+		var new_emails;
+		if ( already_emails == false ) {
+			new_emails = emails.join("\n");
+		} else {
+			new_emails = already_emails + "\n" + emails.join("\n")
+		}
+		document.getElementById('invite-anyone-email-addresses').value = new_emails;
+	}
+} );
