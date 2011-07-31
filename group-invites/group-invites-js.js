27fee192f204c95cdc9a9cf476618a7c05534053
boonebgorges <boonebgorges@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-31 20:14:06 +0000

    Reworks group invite javascript to ensure that checkbox clicking and Remove Invite function properly
    git-svn-id: http://plugins.svn.wordpress.org/invite-anyone/trunk@417680 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- group-invites/group-invites-js.js
+++ group-invites/group-invites-js.js
@@ -19,11 +19,9 @@ jQuery(document).ready( function() {
 	a = j('#send-invite-form #send-to-input').autocomplete(options);
 
 	j("div#invite-anyone-member-list input").click(function() {
-		j('.ajax-loader').toggle();
-
 		var friend_id = j(this).val();
 
-		if ( j(this).attr('checked') == true ) {
+		if ( j(this).prop('checked') == true ) {
 			var friend_action = 'invite';
 		} else {
 			var friend_action = 'uninvite';
@@ -42,8 +40,6 @@ jQuery(document).ready( function() {
 			if ( j("#message") )
 				j("#message").hide();
 
-			j('.ajax-loader').toggle();
-
 			if ( friend_action == 'invite' ) {
 				j('#invite-anyone-invite-list').append(response);
 			} else if ( friend_action == 'uninvite' ) {
@@ -52,33 +48,28 @@ jQuery(document).ready( function() {
 		});
 	});
 
-	j("#invite-anyone-invite-list li a.remove").livequery('click',
-		function() {
-			j('.ajax-loader').toggle();
+	j("#invite-anyone-invite-list li a.remove").click(function() {
+		var friend_id = j(this).prop('id');
 
-			var friend_id = j(this).attr('id');
+		friend_id = friend_id.split('-');
+		friend_id = friend_id[1];
 
-			friend_id = friend_id.split('-');
-			friend_id = friend_id[1];
+		j.post( ajaxurl, {
+			action: 'invite_anyone_groups_invite_user',
+			'friend_action': 'uninvite',
+			'cookie': encodeURIComponent(document.cookie),
+			'_wpnonce': j("input#_wpnonce_invite_uninvite_user").val(),
+			'friend_id': friend_id,
+			'group_id': j("input#group_id").val()
+		},
+		function(response)
+		{
+			j('#invite-anyone-invite-list li#uid-' + friend_id).remove();
+			j('#invite-anyone-member-list input#f-' + friend_id).prop('checked', false);
+		});
 
-			j.post( ajaxurl, {
-				action: 'invite_anyone_groups_invite_user',
-				'friend_action': 'uninvite',
-				'cookie': encodeURIComponent(document.cookie),
-				'_wpnonce': j("input#_wpnonce_invite_uninvite_user").val(),
-				'friend_id': friend_id,
-				'group_id': j("input#group_id").val()
-			},
-			function(response)
-			{
-				j('.ajax-loader').toggle();
-				j('#invite-anyone-invite-list li#uid-' + friend_id).remove();
-				j('#invite-anyone-member-list input#f-' + friend_id).attr('checked', false);
-			});
-
-			return false;
-		}
-	);
+		return false;
+	});
 	
 	j("#invite-anyone-link").click(
 		function() {
@@ -87,7 +78,7 @@ jQuery(document).ready( function() {
 
 			var friend_id = j(this).val();
 
-			if ( j(this).attr('checked') == true ) {
+			if ( j(this).prop('checked') == true ) {
 				var friend_action = 'invite';
 			} else {
 				var friend_action = 'uninvite';
