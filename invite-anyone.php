9c3fa07488011fa831b60aecd1d7b43538edd057
boonebgorges <boonebgorges@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-20 14:11:37 +0000

    Bumps version numbers to 1.0.2
    git-svn-id: http://plugins.svn.wordpress.org/invite-anyone/trunk@440814 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- invite-anyone.php
+++ invite-anyone.php
@@ -3,13 +3,13 @@
 Plugin Name: Invite Anyone
 Plugin URI: http://teleogistic.net/code/buddypress/invite-anyone/
 Description: Allows group admins to invite any BuddyPress member to a group, whether or not they are friends
-Version: 1.0.1
+Version: 1.0.2
 Author: Boone Gorges
 Author URI: http://boonebgorges.com
 */
 
-define( 'BP_INVITE_ANYONE_VER', 	'1.0.1' );
-define( 'BP_INVITE_ANYONE_DB_VER', 	'1.0.1' );
+define( 'BP_INVITE_ANYONE_VER', 	'1.0.2' );
+define( 'BP_INVITE_ANYONE_DB_VER', 	'1.0.2' );
 
 if ( !defined( 'BP_INVITE_ANYONE_SLUG' ) )
 	define( 'BP_INVITE_ANYONE_SLUG', 'invite-anyone' );
