ce0b290ad935e019071a92018a41702df6934a4c
Boone Gorges <boonebgorges@gmail.com>
2011-09-28 15:16:50 -0400

    Adds filters to some settings fields

--- admin/admin-panel.php
+++ admin/admin-panel.php
@@ -256,17 +256,15 @@ function invite_anyone_settings_bypass_registration_lock() {
 }
 
 function invite_anyone_settings_default_invitation_subject() {
-	echo "<textarea name='invite_anyone[default_invitation_subject]' cols=60 rows=2 >" . esc_html( invite_anyone_invitation_subject() ) . "</textarea>";
+	echo apply_filters( 'invite_anyone_settings_default_invitation_subject', "<textarea name='invite_anyone[default_invitation_subject]' cols=60 rows=2 >" . esc_html( invite_anyone_invitation_subject() ) . "</textarea>" );
 }
 
 function invite_anyone_settings_default_invitation_message() {
-	echo "<textarea name='invite_anyone[default_invitation_message]' cols=60 rows=5 >" . esc_html( invite_anyone_invitation_message() ) . "</textarea>";
+	echo apply_filters( 'invite_anyone_settings_default_invitation_message', "<textarea name='invite_anyone[default_invitation_message]' cols=60 rows=5 >" . esc_html( invite_anyone_invitation_message() ) . "</textarea>" );
 }
 
 function invite_anyone_settings_addl_invitation_message() {
-?>
-	<textarea name='invite_anyone[addl_invitation_message]' cols=60 rows=5 ><?php echo esc_html( invite_anyone_process_footer( '[email]' ) ) ?></textarea>
-<?php
+	echo apply_filters( 'invite_anyone_settings_addl_invitation_message', "<textarea name='invite_anyone[addl_invitation_message]' cols=60 rows=5 >" . esc_html( invite_anyone_process_footer( '[email]' ) ) . "</textarea>" );
 }
 
 function invite_anyone_settings_is_customizable() {
