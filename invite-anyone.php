acab9b87f77f0c6f9cc177bb83b7d8ca1614830f
Boone B Gorges <boonebgorges@gmail.com>
2013-09-09 13:46:06 -0400

    Bump version numbers to 1.0.24

--- invite-anyone.php
+++ invite-anyone.php
@@ -3,12 +3,12 @@
 Plugin Name: Invite Anyone
 Plugin URI: http://teleogistic.net/code/buddypress/invite-anyone/
 Description: Allows group admins to invite any member of their BuddyPress community to a group or to the site
-Version: 1.0.23
+Version: 1.0.24
 Author: Boone Gorges
 Author URI: http://boone.gorg.es
 */
-define( 'BP_INVITE_ANYONE_VER', '1.0.23' );
-define( 'BP_INVITE_ANYONE_DB_VER', '1.0.23' );
+define( 'BP_INVITE_ANYONE_VER', '1.0.24' );
+define( 'BP_INVITE_ANYONE_DB_VER', '1.0.24' );
 
 if ( !defined( 'BP_INVITE_ANYONE_SLUG' ) )
 	define( 'BP_INVITE_ANYONE_SLUG', 'invite-anyone' );
--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: boonebgorges, cuny-academic-commons
 Donate link: http://teleogistic.net/donate
 Tags: buddypress, invitations, group, invite, friends, members
 Requires at least: WP 3.2, BuddyPress 1.2.9
-Tested up to: WP 3.5.1, BuddyPress 1.7.2
-Stable tag: 1.0.23
+Tested up to: WP 3.6, BuddyPress 1.8.1
+Stable tag: 1.0.24
 
 Makes BuddyPress's invitation features more powerful.
 
