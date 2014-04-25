183ce84c00341e265111f0acf28307dc94a75d9a
Boone B Gorges <boonebgorges@gmail.com>
2014-04-25 10:15:22 -0400

    Whitespace

--- lib/bbg-cpt-pag.php
+++ lib/bbg-cpt-pag.php
@@ -7,25 +7,25 @@ class BBG_CPT_Pag {
 	 * The CPT query. Defaults to $wp_query; see BBG_CPT_Pag::setup_query()
 	 */
 	var $query;
-	
+
 	/**
 	 * The desired $_GET keys for per_page and paged
 	 */
 	var $get_per_page_key;
 	var $get_paged_key;
-	
+
 	/**
 	 * The values of per_page and paged as retrieved from $_GET
 	 */
 	var $get_per_page;
 	var $get_paged;
-	
+
 	/**
 	 * The number of items found, and the total page number based on this
 	 */
 	var $total_items;
 	var $total_pages;
-	
+
 	/**
 	 * PHP 4 constructor
 	 *
@@ -41,15 +41,15 @@ class BBG_CPT_Pag {
 	 *
 	 * @package BBG CPT Pag
 	 * @since 1.0
-	 */	
-	function __construct( $query = false ) {		
+	 */
+	function __construct( $query = false ) {
 		// Set up the $_GET keys (which are customizable)
 		$this->setup_get_keys();
-		
+
 		// Get the pagination parameters out of $_GET
-		$this->setup_get_params();	
+		$this->setup_get_params();
 	}
-	
+
 	/**
 	 * Sets up query vars.
 	 *
@@ -69,19 +69,19 @@ class BBG_CPT_Pag {
 	 */
 	function setup_query( $query = false ) {
 		global $wp_query;
-		
+
 		if ( !$query )
 			$query =& $wp_query;
-			
+
 		$this->query = $query;
-	
+
 		// Get the total number of items
 		$this->setup_total_items();
-		
+
 		// Get the total number of pages
-		$this->setup_total_pages();	
+		$this->setup_total_pages();
 	}
-	
+
 	/**
 	 * Sets up the $_GET param keys.
 	 *
@@ -95,7 +95,7 @@ class BBG_CPT_Pag {
 		$this->get_per_page_key = apply_filters( 'bbg_cpt_pag_per_page_key', 'per_page' );
 		$this->get_paged_key 	= apply_filters( 'bbg_cpt_pag_paged_key', 'paged' );
 	}
-	
+
 	/**
 	 * Gets params out of $_GET global
 	 *
@@ -107,23 +107,23 @@ class BBG_CPT_Pag {
 	function setup_get_params() {
 		// Per page
 		$per_page = isset( $_GET[$this->get_per_page_key] ) ? $_GET[$this->get_per_page_key] : 10;
-		
+
 		// Basic per_page sanity and security
 		if ( !(int)$per_page )
 			$per_page = 10;
-		
+
 		$this->get_per_page = $per_page;
-		
-		// Page number		
+
+		// Page number
 		$paged = isset( $_GET[$this->get_paged_key] ) ? $_GET[$this->get_paged_key] : 1;
-		
+
 		// Basic paged sanity and security
 		if ( !(int)$paged )
 			$paged = 1;
-		
+
 		$this->get_paged = $paged;
 	}
-	
+
 	/**
 	 * Get the total number of items out of the query
 	 *
@@ -133,7 +133,7 @@ class BBG_CPT_Pag {
 	function setup_total_items() {
 		$this->total_items = $this->query->found_posts;
 	}
-	
+
 	/**
 	 * Get the total number of pages out of the query
 	 *
@@ -143,7 +143,7 @@ class BBG_CPT_Pag {
 	function setup_total_pages() {
 		$this->total_pages = $this->query->max_num_pages;
 	}
-	
+
 	/**
 	 * Get the start number for the current view (ie "Viewing *5* - 8 of 12")
 	 *
@@ -155,12 +155,12 @@ class BBG_CPT_Pag {
 	 *
 	 * @return int $start The start number
 	 */
-	function get_start_number() {		
+	function get_start_number() {
 		$start = ( ( $this->get_paged - 1 ) * $this->get_per_page ) + 1;
-		
+
 		return $start;
 	}
-	
+
 	/**
 	 * Get the end number for the current view (ie "Viewing 5 - *8* of 12")
 	 *
@@ -175,15 +175,15 @@ class BBG_CPT_Pag {
 	 */
 	function get_end_number() {
 		global $wp_query;
-		
+
 		$end = $this->get_paged * $this->get_per_page;
-		
+
 		if ( $end > $this->total_items )
 			$end = $this->total_items;
-		
+
 		return $end;
 	}
-	
+
 	/**
 	 * Return or echo the "Viewing x-y of z" message
 	 *
@@ -193,7 +193,7 @@ class BBG_CPT_Pag {
 	 * @param str $type Optional. 'echo' will echo the results, anything else will return them
 	 * @return str $page_links The "viewing" text
 	 */
-	function currently_viewing_text( $type = 'echo' ) {	 
+	function currently_viewing_text( $type = 'echo' ) {
 		$start  = $this->get_start_number();
 		$end	= $this->get_end_number();
 		
@@ -204,7 +204,7 @@ class BBG_CPT_Pag {
 		else
 			return $string;
 	}
-	
+
 	/**
 	 * Return or echo the pagination links
 	 *
@@ -214,7 +214,7 @@ class BBG_CPT_Pag {
 	 * @param str $type Optional. 'echo' will echo the results, anything else will return them
 	 * @return str $page_links The pagination links
 	 */
-	function paginate_links( $type = 'echo' ) {	 
+	function paginate_links( $type = 'echo' ) {
 		$page_links = paginate_links( array(
 			'base' 		=> add_query_arg( $this->get_paged_key, '%#%' ),
 			'format' 	=> '',
@@ -224,7 +224,7 @@ class BBG_CPT_Pag {
 			'current' 	=> $this->get_paged,
 			'add_args'	=> array( $this->get_per_page_key => $this->get_per_page )
 		));
-		
+
 		if ( 'echo' == $type )
 			echo $page_links;
 		else
