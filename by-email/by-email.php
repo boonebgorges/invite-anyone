9d3d061bafc5f4b2f6444d0e6875554b83421ff1
David Cavins <david.cavins@gmail.com>
2014-05-22 15:00:30 -0500

    Enforce group meta setting "invite_status"
    Uses BuddyPress group meta setting "invite_status" to determine whether
    user should have access to send group invites. Also adds check in
    creation of group list from user's "send invites" pane.

--- by-email/by-email.php
+++ by-email/by-email.php
@@ -646,7 +646,11 @@ function invite_anyone_screen_one_content() {
 			<li>
 				<p><?php _e( '(optional) Select some groups. Invitees will receive invitations to these groups when they join the site.', 'bp-invite-anyone' ) ?></p>
 				<ul id="invite-anyone-group-list">
-					<?php while ( bp_groups() ) : bp_the_group(); ?>
+					<?php while ( bp_groups() ) : bp_the_group(); 
+					if ( invite_anyone_group_invite_access_test( bp_get_group_id() ) != 'anyone' )
+						continue;
+					?>
+
 						<li>
 						<input type="checkbox" name="invite_anyone_groups[]" id="invite_anyone_groups-<?php bp_group_id() ?>" value="<?php bp_group_id() ?>" <?php if ( $from_group == bp_get_group_id() || array_search( bp_get_group_id(), $returned_groups) ) : ?>checked<?php endif; ?> />
 
