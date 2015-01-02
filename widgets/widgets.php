bfd21e2088f0381985c247cd231857c3ff747d94
Boone Gorges <boonebgorges@gmail.com>
2015-01-02 15:24:11 +0000

    Avoid PHP notices when creating new widget.

--- widgets/widgets.php
+++ widgets/widgets.php
@@ -81,16 +81,19 @@ class InviteAnyoneWidget extends WP_Widget {
 
     /** @see WP_Widget::form */
     function form($instance) {
-    	
-        if ( !$title = esc_attr($instance['title']) )
-        	$title = __( 'Invite Anyone', 'bp-invite-anyone' );
-        
-        if ( !$email_fields = (int)$instance['email_fields'] )
+
+	if ( ! isset( $instance['title'] ) || ! $title = esc_attr( $instance['title'] ) ) {
+        	$title = _ 'Invite Anyone', 'bp-invite-anyone' );
+	}
+
+        if ( ! isset( $instance['email_fields'] ) || ! $email_fields = (int) $instance['email_fields'] ) {
         	$email_fields = 3;
-        	
-        if ( !$instruction_text = esc_attr( $instance['instruction_text'] ) )
+	}
+
+        if ( ! isset( $instance['instruction_text'] ) || ! $instruction_text = esc_attr( $instance['instruction_text'] ) ) {
         	$instruction_text = __( 'Invite friends to join the site by entering their email addresses below.', 'bp-invite-anyone' );
-        	
+	}
+
         ?>
             <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></label></p>
            
