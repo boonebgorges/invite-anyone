60d3659e2035266ce8322b14ede62b3029a5888d
Boone B Gorges <boonebgorges@gmail.com>
2015-01-02 10:30:39 -0500

    Use BP's factory method for creating test users.

--- tests/testcases/test-sample.php
+++ tests/testcases/test-sample.php
@@ -18,7 +18,7 @@ class Invite_Anyone_Tests extends BP_UnitTestCase {
 		buddypress()->current_component = buddypress()->groups->id;
 		buddypress()->current_action = 'create';
 
-		$u = $this->create_user();
+		$u = $this->factory->user->create();
 		$this->assertSame( 'anyone', invite_anyone_group_invite_access_test( 0, $u ) );
 
 		buddypress()->current_component = $cc;
@@ -43,7 +43,7 @@ class Invite_Anyone_Tests extends BP_UnitTestCase {
 	 * @group invite_anyone_group_invite_access_test
 	 */
 	public function test_group_access_test_not_a_member_of_group() {
-		$u = $this->create_user();
+		$u = $this->factory->user->create();
 		$g = $this->factory->group->create();
 
 		$this->assertSame( 'noone', invite_anyone_group_invite_access_test( $g, $u ) );
@@ -66,20 +66,20 @@ class Invite_Anyone_Tests extends BP_UnitTestCase {
 
 		$g = $this->factory->group->create();
 
-		$u1 = $this->create_user( array(
+		$u1 = $this->factory->user->create( array(
 			'role' => 'administrator',
 		) );
 		$this->add_user_to_group( $u1, $g );
 
-		$u2 = $this->create_user();
+		$u2 = $this->factory->user->create();
 		$this->add_user_to_group( $u2, $g );
 
-		$u3 = $this->create_user();
+		$u3 = $this->factory->user->create();
 		$this->add_user_to_group( $u3, $g );
 		$m3 = new BP_Groups_Member( $u3, $g );
 		$m3->promote( 'mod' );
 
-		$u4 = $this->create_user();
+		$u4 = $this->factory->user->create();
 		$this->add_user_to_group( $u4, $g );
 		$m4 = new BP_Groups_Member( $u4, $g );
 		$m4->promote( 'admin' );
