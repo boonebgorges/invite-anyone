55a90b3db1f5b6085323a8db3f54941582686a2c
Boone B Gorges <boonebgorges@gmail.com>
2024-08-01 06:58:17 -0500

    Codestyling in group-invites/templates/invite-anyone.php.

--- group-invites/templates/invite-anyone.php
+++ group-invites/templates/invite-anyone.php
@@ -14,17 +14,21 @@ if ( function_exists( 'bp_post_get_permalink' ) ) { // ugly ugly ugly hack to ch
 	add_action( 'wp_footer', 'invite_anyone_add_old_css' );
 	?>
 
-	<?php if ( bp_has_groups() ) : while ( bp_groups() ) : bp_the_group(); ?>
+	<?php
+	if ( bp_has_groups() ) :
+		while ( bp_groups() ) :
+			bp_the_group();
+			?>
 
-		<?php do_action( 'bp_before_group_send_invites_content' ) ?>
+					<?php do_action( 'bp_before_group_send_invites_content' ); ?>
 
-			<?php if ( $event != 'create' ) : ?>
-				<form action="<?php bp_group_send_invite_form_action() ?>" method="post" id="send-invite-form">
+					<?php if ( $event != 'create' ) : ?>
+				<form action="<?php bp_group_send_invite_form_action(); ?>" method="post" id="send-invite-form">
 			<?php endif; ?>
 
 				<div class="left-menu">
 
-					<p><?php _e("Search for members to invite:", 'invite-anyone') ?></span></p>
+					<p><?php _e( 'Search for members to invite:', 'invite-anyone' ); ?></span></p>
 
 					<ul class="first acfb-holder">
 						<li>
@@ -32,14 +36,14 @@ if ( function_exists( 'bp_post_get_permalink' ) ) { // ugly ugly ugly hack to ch
 						</li>
 					</ul>
 
-					<p><?php _e( 'Select members from the directory:', 'invite-anyone' ) ?> </p>
+					<p><?php _e( 'Select members from the directory:', 'invite-anyone' ); ?> </p>
 
 					<div id="invite-anyone-member-list">
 						<ul>
-							<?php bp_new_group_invite_member_list() ?>
+							<?php bp_new_group_invite_member_list(); ?>
 						</ul>
 
-						<?php wp_nonce_field( 'groups_invite_uninvite_user', '_wpnonce_invite_uninvite_user' ) ?>
+						<?php wp_nonce_field( 'groups_invite_uninvite_user', '_wpnonce_invite_uninvite_user' ); ?>
 					</div>
 
 				</div>
@@ -47,29 +51,32 @@ if ( function_exists( 'bp_post_get_permalink' ) ) { // ugly ugly ugly hack to ch
 				<div class="main-column">
 
 					<div id="message" class="info">
-						<p><?php _e('Select people to invite.', 'invite-anyone'); ?></p>
+						<p><?php _e( 'Select people to invite.', 'invite-anyone' ); ?></p>
 					</div>
 
-					<?php do_action( 'bp_before_group_send_invites_list' ) ?>
+					<?php do_action( 'bp_before_group_send_invites_list' ); ?>
 
 					<?php /* The ID 'friend-list' is important for AJAX support. */ ?>
 					<ul id="invite-anyone-invite-list" class="item-list">
 					<?php if ( bp_group_has_invites() ) : ?>
 
-						<?php while ( bp_group_invites() ) : bp_group_the_invite(); ?>
+						<?php
+						while ( bp_group_invites() ) :
+							bp_group_the_invite();
+							?>
 
-							<li id="<?php bp_group_invite_item_id() ?>">
-								<?php bp_group_invite_user_avatar() ?>
+							<li id="<?php bp_group_invite_item_id(); ?>">
+								<?php bp_group_invite_user_avatar(); ?>
 
-								<h4><?php bp_group_invite_user_link() ?></h4>
-								<span class="activity"><?php bp_group_invite_user_last_active() ?></span>
+								<h4><?php bp_group_invite_user_link(); ?></h4>
+								<span class="activity"><?php bp_group_invite_user_last_active(); ?></span>
 
-								<?php do_action( 'bp_group_send_invites_item' ) ?>
+								<?php do_action( 'bp_group_send_invites_item' ); ?>
 
 								<div class="action">
-									<a class="remove" href="<?php bp_group_invite_user_remove_invite_url() ?>" id="<?php bp_group_invite_item_id() ?>"><?php _e( 'Remove Invite', 'invite-anyone' ) ?></a>
+									<a class="remove" href="<?php bp_group_invite_user_remove_invite_url(); ?>" id="<?php bp_group_invite_item_id(); ?>"><?php _e( 'Remove Invite', 'invite-anyone' ); ?></a>
 
-									<?php do_action( 'bp_group_send_invites_item_action' ) ?>
+									<?php do_action( 'bp_group_send_invites_item_action' ); ?>
 								</div>
 							</li>
 
@@ -77,25 +84,27 @@ if ( function_exists( 'bp_post_get_permalink' ) ) { // ugly ugly ugly hack to ch
 					<?php endif; ?>
 					</ul>
 
-					<?php do_action( 'bp_after_group_send_invites_list' ) ?>
+							<?php do_action( 'bp_after_group_send_invites_list' ); ?>
 
 				</div>
 
 				<div class="clear"></div>
 
-			<?php if ( $event != 'create' ) : ?>
-				<p class="clear"><input type="submit" name="submit" id="submit" value="<?php _e( 'Send Invites', 'invite-anyone' ) ?>" /></p>
-				<?php wp_nonce_field( 'groups_send_invites', '_wpnonce_send_invites') ?>
+					<?php if ( $event != 'create' ) : ?>
+				<p class="clear"><input type="submit" name="submit" id="submit" value="<?php _e( 'Send Invites', 'invite-anyone' ); ?>" /></p>
+						<?php wp_nonce_field( 'groups_send_invites', '_wpnonce_send_invites' ); ?>
 			<?php endif; ?>
 
-			<input type="hidden" name="group_id" id="group_id" value="<?php bp_group_id() ?>" />
+			<input type="hidden" name="group_id" id="group_id" value="<?php bp_group_id(); ?>" />
 
-			<?php if ( $event != 'create' ) : ?>
+					<?php if ( $event != 'create' ) : ?>
 				</form>
 			<?php endif; ?>
 
-		<?php do_action( 'bp_before_group_send_invites_content' ); ?>
-	<?php endwhile; endif;
+					<?php do_action( 'bp_before_group_send_invites_content' ); ?>
+			<?php
+	endwhile;
+endif;
 
 } else { // Begin BP 1.2 code
 	$invitation_url = bp_members_get_user_url(
@@ -105,18 +114,18 @@ if ( function_exists( 'bp_post_get_permalink' ) ) { // ugly ugly ugly hack to ch
 
 	?>
 
-	<?php do_action( 'bp_before_group_send_invites_content' ) ?>
+	<?php do_action( 'bp_before_group_send_invites_content' ); ?>
 
-	<?php if ( invite_anyone_access_test() && !bp_is_group_create() ) : ?>
-		<p><?php _e( 'Want to invite someone to the group who is not yet a member of the site?', 'invite-anyone' ) ?> <a href="<?php echo esc_url( $invitation_url ); ?>"><?php _e( 'Send invitations by email.', 'invite-anyone' ) ?></a></p>
+	<?php if ( invite_anyone_access_test() && ! bp_is_group_create() ) : ?>
+		<p><?php _e( 'Want to invite someone to the group who is not yet a member of the site?', 'invite-anyone' ); ?> <a href="<?php echo esc_url( $invitation_url ); ?>"><?php _e( 'Send invitations by email.', 'invite-anyone' ); ?></a></p>
 	<?php endif; ?>
 
-	<?php if ( !bp_get_new_group_id() ) : ?>
-		<form action="<?php invite_anyone_group_invite_form_action() ?>" method="post" id="send-invite-form">
+	<?php if ( ! bp_get_new_group_id() ) : ?>
+		<form action="<?php invite_anyone_group_invite_form_action(); ?>" method="post" id="send-invite-form">
 	<?php endif; ?>
 
 	<div class="left-menu">
-		<p><?php _e("Search for members to invite:", 'invite-anyone') ?></p>
+		<p><?php _e( 'Search for members to invite:', 'invite-anyone' ); ?></p>
 
 		<ul class="first acfb-holder">
 			<li>
@@ -124,14 +133,14 @@ if ( function_exists( 'bp_post_get_permalink' ) ) { // ugly ugly ugly hack to ch
 			</li>
 		</ul>
 
-		<?php wp_nonce_field( 'groups_invite_uninvite_user', '_wpnonce_invite_uninvite_user' ) ?>
+		<?php wp_nonce_field( 'groups_invite_uninvite_user', '_wpnonce_invite_uninvite_user' ); ?>
 
 		<?php if ( ! invite_anyone_is_large_network( 'users' ) ) : ?>
-			<p><?php _e( 'Select members from the directory:', 'invite-anyone' ) ?></p>
+			<p><?php _e( 'Select members from the directory:', 'invite-anyone' ); ?></p>
 
 			<div id="invite-anyone-member-list">
 				<ul>
-					<?php bp_new_group_invite_member_list() ?>
+					<?php bp_new_group_invite_member_list(); ?>
 				</ul>
 			</div>
 		<?php endif ?>
@@ -140,29 +149,32 @@ if ( function_exists( 'bp_post_get_permalink' ) ) { // ugly ugly ugly hack to ch
 	<div class="main-column">
 
 		<div id="message" class="info">
-			<p><?php _e('Select people to invite from your friends list.', 'invite-anyone'); ?></p>
+			<p><?php _e( 'Select people to invite from your friends list.', 'invite-anyone' ); ?></p>
 		</div>
 
-		<?php do_action( 'bp_before_group_send_invites_list' ) ?>
+		<?php do_action( 'bp_before_group_send_invites_list' ); ?>
 
 		<?php /* The ID 'friend-list' is important for AJAX support. */ ?>
 		<ul id="invite-anyone-invite-list" class="item-list">
 		<?php if ( bp_group_has_invites() ) : ?>
 
-			<?php while ( bp_group_invites() ) : bp_group_the_invite(); ?>
+			<?php
+			while ( bp_group_invites() ) :
+				bp_group_the_invite();
+				?>
 
-				<li id="<?php bp_group_invite_item_id() ?>">
-					<?php bp_group_invite_user_avatar() ?>
+				<li id="<?php bp_group_invite_item_id(); ?>">
+					<?php bp_group_invite_user_avatar(); ?>
 
-					<h4><?php bp_group_invite_user_link() ?></h4>
-					<span class="activity"><?php bp_group_invite_user_last_active() ?></span>
+					<h4><?php bp_group_invite_user_link(); ?></h4>
+					<span class="activity"><?php bp_group_invite_user_last_active(); ?></span>
 
-					<?php do_action( 'bp_group_send_invites_item' ) ?>
+					<?php do_action( 'bp_group_send_invites_item' ); ?>
 
 					<div class="action">
-						<a class="remove" href="<?php bp_group_invite_user_remove_invite_url() ?>" id="<?php bp_group_invite_item_id() ?>"><?php _e( 'Remove Invite', 'invite-anyone' ) ?></a>
+						<a class="remove" href="<?php bp_group_invite_user_remove_invite_url(); ?>" id="<?php bp_group_invite_item_id(); ?>"><?php _e( 'Remove Invite', 'invite-anyone' ); ?></a>
 
-						<?php do_action( 'bp_group_send_invites_item_action' ) ?>
+						<?php do_action( 'bp_group_send_invites_item_action' ); ?>
 					</div>
 				</li>
 
@@ -171,37 +183,41 @@ if ( function_exists( 'bp_post_get_permalink' ) ) { // ugly ugly ugly hack to ch
 		<?php endif; ?>
 		</ul>
 
-		<?php do_action( 'bp_after_group_send_invites_list' ) ?>
+		<?php do_action( 'bp_after_group_send_invites_list' ); ?>
 
 	</div>
 
 	<div class="clear"></div>
 
-	<?php if ( !bp_get_new_group_id() ) : ?>
+	<?php if ( ! bp_get_new_group_id() ) : ?>
 	<div class="submit">
-		<input type="submit" name="submit" id="submit" value="<?php _e( 'Send Invites', 'invite-anyone' ) ?>" />
+		<input type="submit" name="submit" id="submit" value="<?php _e( 'Send Invites', 'invite-anyone' ); ?>" />
 	</div>
 	<?php endif; ?>
 
-	<?php wp_nonce_field( 'groups_send_invites', '_wpnonce_send_invites') ?>
+	<?php wp_nonce_field( 'groups_send_invites', '_wpnonce_send_invites' ); ?>
 
 		<!-- Don't leave out this sweet field -->
 	<?php
-	if ( !bp_get_new_group_id() ) {
-		?><input type="hidden" name="group_id" id="group_id" value="<?php bp_group_id() ?>" /><?php
+	if ( ! bp_get_new_group_id() ) {
+		?>
+		<input type="hidden" name="group_id" id="group_id" value="<?php bp_group_id(); ?>" />
+		<?php
 	} else {
-		?><input type="hidden" name="group_id" id="group_id" value="<?php bp_new_group_id() ?>" /><?php
+		?>
+		<input type="hidden" name="group_id" id="group_id" value="<?php bp_new_group_id(); ?>" />
+		<?php
 	}
 	?>
 
-	<?php if ( !bp_get_new_group_id() ) : ?>
+	<?php if ( ! bp_get_new_group_id() ) : ?>
 		</form>
 	<?php endif; ?>
 
-	<?php do_action( 'bp_after_group_send_invites_content' ) ?>
+	<?php do_action( 'bp_after_group_send_invites_content' ); ?>
 
 
 
-<?php
+	<?php
 }
 ?>
