0b2cd4c08827c9058c4c4d92373b2fe83461cda8
Boone B Gorges <boonebgorges@gmail.com>
2024-08-01 07:03:31 -0500

    Whitespace.

--- group-invites/templates/invite-anyone.php
+++ group-invites/templates/invite-anyone.php
@@ -20,88 +20,87 @@ if ( function_exists( 'bp_post_get_permalink' ) ) { // ugly ugly ugly hack to ch
 			bp_the_group();
 			?>
 
-					<?php do_action( 'bp_before_group_send_invites_content' ); ?>
+			<?php do_action( 'bp_before_group_send_invites_content' ); ?>
 
-					<?php if ( 'create' !== $event ) : ?>
+			<?php if ( 'create' !== $event ) : ?>
 				<form action="<?php bp_group_send_invite_form_action(); ?>" method="post" id="send-invite-form">
 			<?php endif; ?>
 
-				<div class="left-menu">
+			<div class="left-menu">
 
-					<p><?php esc_html_e( 'Search for members to invite:', 'invite-anyone' ); ?></span></p>
+				<p><?php esc_html_e( 'Search for members to invite:', 'invite-anyone' ); ?></span></p>
 
-					<ul class="first acfb-holder">
-						<li>
-							<input type="text" name="send-to-input" class="send-to-input" id="send-to-input" />
+				<ul class="first acfb-holder">
+					<li>
+						<input type="text" name="send-to-input" class="send-to-input" id="send-to-input" />
+					</li>
+				</ul>
+
+				<p><?php esc_html_e( 'Select members from the directory:', 'invite-anyone' ); ?> </p>
+
+				<div id="invite-anyone-member-list">
+					<ul>
+						<?php bp_new_group_invite_member_list(); ?>
+					</ul>
+
+					<?php wp_nonce_field( 'groups_invite_uninvite_user', '_wpnonce_invite_uninvite_user' ); ?>
+				</div>
+			</div>
+
+			<div class="main-column">
+
+				<div id="message" class="info">
+					<p><?php esc_html_e( 'Select people to invite.', 'invite-anyone' ); ?></p>
+				</div>
+
+				<?php do_action( 'bp_before_group_send_invites_list' ); ?>
+
+				<?php /* The ID 'friend-list' is important for AJAX support. */ ?>
+				<ul id="invite-anyone-invite-list" class="item-list">
+				<?php if ( bp_group_has_invites() ) : ?>
+
+					<?php
+					while ( bp_group_invites() ) :
+						bp_group_the_invite();
+						?>
+
+						<li id="<?php bp_group_invite_item_id(); ?>">
+							<?php bp_group_invite_user_avatar(); ?>
+
+							<h4><?php bp_group_invite_user_link(); ?></h4>
+							<span class="activity"><?php bp_group_invite_user_last_active(); ?></span>
+
+							<?php do_action( 'bp_group_send_invites_item' ); ?>
+
+							<div class="action">
+								<a class="remove" href="<?php bp_group_invite_user_remove_invite_url(); ?>" id="<?php bp_group_invite_item_id(); ?>"><?php esc_html_e( 'Remove Invite', 'invite-anyone' ); ?></a>
+
+								<?php do_action( 'bp_group_send_invites_item_action' ); ?>
+							</div>
 						</li>
-					</ul>
 
-					<p><?php esc_html_e( 'Select members from the directory:', 'invite-anyone' ); ?> </p>
+					<?php endwhile; ?>
+				<?php endif; ?>
+				</ul>
 
-					<div id="invite-anyone-member-list">
-						<ul>
-							<?php bp_new_group_invite_member_list(); ?>
-						</ul>
+				<?php do_action( 'bp_after_group_send_invites_list' ); ?>
 
-						<?php wp_nonce_field( 'groups_invite_uninvite_user', '_wpnonce_invite_uninvite_user' ); ?>
-					</div>
+			</div>
 
-				</div>
+			<div class="clear"></div>
 
-				<div class="main-column">
-
-					<div id="message" class="info">
-						<p><?php esc_html_e( 'Select people to invite.', 'invite-anyone' ); ?></p>
-					</div>
-
-					<?php do_action( 'bp_before_group_send_invites_list' ); ?>
-
-					<?php /* The ID 'friend-list' is important for AJAX support. */ ?>
-					<ul id="invite-anyone-invite-list" class="item-list">
-					<?php if ( bp_group_has_invites() ) : ?>
-
-						<?php
-						while ( bp_group_invites() ) :
-							bp_group_the_invite();
-							?>
-
-							<li id="<?php bp_group_invite_item_id(); ?>">
-								<?php bp_group_invite_user_avatar(); ?>
-
-								<h4><?php bp_group_invite_user_link(); ?></h4>
-								<span class="activity"><?php bp_group_invite_user_last_active(); ?></span>
-
-								<?php do_action( 'bp_group_send_invites_item' ); ?>
-
-								<div class="action">
-									<a class="remove" href="<?php bp_group_invite_user_remove_invite_url(); ?>" id="<?php bp_group_invite_item_id(); ?>"><?php esc_html_e( 'Remove Invite', 'invite-anyone' ); ?></a>
-
-									<?php do_action( 'bp_group_send_invites_item_action' ); ?>
-								</div>
-							</li>
-
-						<?php endwhile; ?>
-					<?php endif; ?>
-					</ul>
-
-							<?php do_action( 'bp_after_group_send_invites_list' ); ?>
-
-				</div>
-
-				<div class="clear"></div>
-
-					<?php if ( 'create' !== $event ) : ?>
+			<?php if ( 'create' !== $event ) : ?>
 				<p class="clear"><input type="submit" name="submit" id="submit" value="<?php esc_html_e( 'Send Invites', 'invite-anyone' ); ?>" /></p>
-						<?php wp_nonce_field( 'groups_send_invites', '_wpnonce_send_invites' ); ?>
+				<?php wp_nonce_field( 'groups_send_invites', '_wpnonce_send_invites' ); ?>
 			<?php endif; ?>
 
 			<input type="hidden" name="group_id" id="group_id" value="<?php bp_group_id(); ?>" />
 
-					<?php if ( 'create' !== $event ) : ?>
+			<?php if ( 'create' !== $event ) : ?>
 				</form>
 			<?php endif; ?>
 
-					<?php do_action( 'bp_before_group_send_invites_content' ); ?>
+			<?php do_action( 'bp_before_group_send_invites_content' ); ?>
 			<?php
 	endwhile;
 endif;
@@ -216,8 +215,5 @@ endif;
 
 	<?php do_action( 'bp_after_group_send_invites_content' ); ?>
 
-
-
 	<?php
 }
-?>
